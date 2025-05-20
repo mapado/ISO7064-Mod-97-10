627a7badaebbdf9facd6a100a16fdd7d14b16643
Julien Deniau <julien.deniau@gmail.com>
2025-05-20 15:06:42 +0000

    Migrate to composer and src

--- Tests/MainTest.php
+++ Tests/MainTest.php
@@ -1,23 +1,23 @@
 <?php
 
-require 'ISO7064Mod97_10.php';
+use Mapado\Iso7064Mod9710\ISO7064Mod97_10;
+use PHPUnit\Framework\TestCase;
 
-class MainTest extends PHPUnit_Framework_TestCase
+class MainTest extends TestCase
 {
   public function testNumbers()
-    {
+  {
 
-      $c = new ISO7064Mod97_10();
-      $n = 107571;
-      $validNumber = '10757107';
-      $invalidNumber = '10767107';
-      
+    $c = new ISO7064Mod97_10();
+    $n = 107571;
+    $validNumber = '10757107';
+    $invalidNumber = '10767107';
 
-      $this->assertEquals( $validNumber, $c->encode($n) );
-      $this->assertNotEquals( $invalidNumber, $c->encode($n) );
-      
-      $this->assertTrue( $c->verify($validNumber) );
-      $this->assertFalse( $c->verify($invalidNumber) );
-      
-    }
+
+    $this->assertEquals($validNumber, $c->encode($n));
+    $this->assertNotEquals($invalidNumber, $c->encode($n));
+
+    $this->assertTrue($c->verify($validNumber));
+    $this->assertFalse($c->verify($invalidNumber));
+  }
 }
